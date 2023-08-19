011bda5a44ca7c17d0b9451acb2165593a5962f6
BahodirZIyodali <bahodirziyodaliyev@gmail.com>
2023-08-19 15:24:37 +0500

    intro animation added

--- src/pages/intro.jsx
+++ src/pages/intro.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import Bg from '../assets/images/div.hero-removebg.png'
+import React, { useState, useEffect } from 'react';
+import Bg from '../assets/images/div.hero-removebg.png';
+
 const Intro = () => {
+  const texts = ['onlayn', 'mutaxasislikni', 'Najot Talimda'];
+  const [textIndex, setTextIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 4000); 
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section id="intro">
       <div className="intro_text">
         <h1>
-          Kasblarni <br />
-          <span>onlayn</span> <br />
+        kasblarni
+          {texts.map((text, index) => (
+            <span
+              key={index}
+              className={`animated-text ${
+                textIndex === index ? 'show' : 'hide'
+              }`}
+              style={{ animationDelay: `${index * 0.9}s` }} 
+            >
+              {text}
+            </span>
+          ))}
+          <br />
           o‘rganing
         </h1>
 
